689570483265054cd5d4d530698b1905d018df97
Don_Chiang <Don_Chiang@asus.com>
2016-02-24 17:03:18 +0800

    add new block

--- sample.js
+++ sample.js
@@ -12,26 +12,26 @@
 
     // For information on writing Scratch extensions, see the ScratchX wiki:
     // https://github.com/LLK/scratchx/wiki#writing-extensions-for-scratchx
-    ext.doSomething = function() {
+    ext.say = function() {
         // code to do something goes here
     };
 	
 
 	
-	ext.tobySay = function() {
+	ext.weatherBroadcast = function() {
 	
 	};
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            [' ', 'do something', 'doSomething'],
-			['', 'Toby Say %n ', 'tobySay', 'hi']
+            [' ', 'Weather Broadcast', 'weatherBroadcast'],
+			['', 'Toby Say %n ', 'say', 'hello, i\'m toby']
 
         ],
         url: 'http://' // Link to extension documentation, homepage, etc.
     };
 
     // Register the extension
-    ScratchExtensions.register('Sample extension', descriptor, ext);
+    ScratchExtensions.register('Toby', descriptor, ext);
 })({});
