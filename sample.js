2eb587786d7b8c0dcb4bca508f122c96105fbc0e
Russell Fair <rfair404@gmail.com>
2016-03-15 22:39:15 -0400

    rewrite all the current post stuff

--- sample.js
+++ sample.js
@@ -1,6 +1,6 @@
 (function(ext) {
     var current_user = 1;
-    var current_post = 1;
+    var current_post_id = 1;
     var api_base = '';
     var api_namespace = '';
     var api_endpoint = '';
@@ -108,64 +108,77 @@
 
     ext.set_current_post_id = function(value) {
       console.log('current post set to '+ value);
-      this.current_post = value;
+      this.current_post_id = value;
     }
 
     ext.get_current_post_id = function() {
       // console.log(this.current_post);
-      return this.current_post;
+      return this.current_post_id;
     }
-    //im here
 
-    ext.get_next_post_id = function(callback) {
+    ext.get_post = function(post_data, callback) {
+      // console.log(post_data);
         // Make an AJAX call to the WordPress REST API to get site description
         $.ajax({
-          url: 'http://rest-api-demo.q21.co/wp-json/wp/v2/posts?per_page=1&page='+post_start+'',
-          dataType: 'json',
-          success: function(ret){
-            console.log(ret);
-            callback(ret[0]['id']);
-          }
-        });
-      post_start++;
-    };
-
-    ext.get_post_ids = function(callback) {
-      //handle pagination somehow???
-
-      // Make an AJAX call to the WordPress REST API to get a collection of post ids
-      $.ajax({
-         url: 'http://rest-api-demo.q21.co/wp-json/wp/v2/posts/',
-        dataType: 'json',
-              success: function(ret){
-                var post_ids = [];
-                for (var i = ret.length - 1; i >= 0; i--) {
-                  post_ids.push(ret[i]['id']);
-                };
-                callback(post_ids);
-              }
-        });
-    };
-
-    ext.get_post_by_id = function(callback) {
-          // @TODO handle pagination somehow???
-          console.log(post_id);
-        // Make an AJAX call to the WordPress REST API to get a collection of post ids
-        $.ajax({
-              url: 'http://rest-api-demo.q21.co/wp-json/wp/v2/posts/'+post_id+'',
+              url: this.get_api_base() + this.get_api_namespace() + '/posts/' + this.get_current_post_id(),
               dataType: 'json',
               success: function(ret){
-                console.log(ret);
-                callback('yepp');
+                // console.log(ret);
+                callback(ret[post_data]);
               }
         });
     };
+    //im here
+
+    // ext.get_next_post_id = function(callback) {
+    //     // Make an AJAX call to the WordPress REST API to get site description
+    //     $.ajax({
+    //       url: 'http://rest-api-demo.q21.co/wp-json/wp/v2/posts?per_page=1&page='+post_start+'',
+    //       dataType: 'json',
+    //       success: function(ret){
+    //         console.log(ret);
+    //         callback(ret[0]['id']);
+    //       }
+    //     });
+    //   post_start++;
+    // };
+
+    // ext.get_post_ids = function(callback) {
+    //   //handle pagination somehow???
+
+    //   // Make an AJAX call to the WordPress REST API to get a collection of post ids
+    //   $.ajax({
+    //      url: 'http://rest-api-demo.q21.co/wp-json/wp/v2/posts/',
+    //     dataType: 'json',
+    //           success: function(ret){
+    //             var post_ids = [];
+    //             for (var i = ret.length - 1; i >= 0; i--) {
+    //               post_ids.push(ret[i]['id']);
+    //             };
+    //             callback(post_ids);
+    //           }
+    //     });
+    // };
+
+    // ext.get_post_by_id = function(callback) {
+    //       // @TODO handle pagination somehow???
+    //       console.log(post_id);
+    //     // Make an AJAX call to the WordPress REST API to get a collection of post ids
+    //     $.ajax({
+    //           url: 'http://rest-api-demo.q21.co/wp-json/wp/v2/posts/'+post_id+'',
+    //           dataType: 'json',
+    //           success: function(ret){
+    //             console.log(ret);
+    //             callback('yepp');
+    //           }
+    //     });
+    // };
 
 
-    ext.set_post = function(post, value) {
-      post_start = value;
-      console.log('New "next post" is '+post_start);
-    }
+    // ext.set_post = function(post, value) {
+    //   post_start = value;
+    //   console.log('New "next post" is '+post_start);
+    // }
 
     // Block and block menu descriptions
     var descriptor = {
@@ -180,10 +193,11 @@
             ['R', 'Site Description', 'get_site_description'],//new
             ['R', 'Log API Endpoint Data', 'get_wp_v2_data'],//new
             ['R', 'Get User %m.user_id', 'get_user', 'id'],//new
+            ['R', 'Get Post %m.post_id', 'get_post', 'id'],
 
             // ['R', 'Get Post IDs', 'get_post_ids', 1],
             // ['R', 'Get Post by ID', 'get_post_by_id', 'post_now'],
-            // ['R', 'Next Post', 'get_next_post_id', 'post_now'],
+            // ['R', 'Get Post', 'get_next_post_id', 'post_now'],
             // [' ', 'Set %m.post_pagination to %n', 'set_post', 'post_start', '1'],
             // [' ', 'Access', 'access_scratch_dataset'],
 
@@ -192,6 +206,8 @@
           post_pagination: ['post_next', 'post_previous'],
           api_base: ['api_base', 'api_namespace', 'api_endpoint'],
           user_id: ['id', 'name', 'description', 'avatar_urls'],
+          post_id: ['id', 'date', 'title', 'content'],
+
         },
         url: 'http://rfair404.github.io/WP-REST-API-FOR-SCRATCHX' 
         // Link to extension documentation, homepage, etc.
