fa8d514e69b604c5bb0e26bb8f8d27c57e34081e
Russell Fair <rfair404@gmail.com>
2016-03-15 20:46:12 -0400

    debug title

--- sample.js
+++ sample.js
@@ -28,7 +28,7 @@
 
     ext.get_site_title = function(callback, api_base, api_namespace) {
           // Make an AJAX call to the WordPress REST API for the site title
-          console.log(get_api_base);
+          console.log(get_api_base());
           $.ajax({
                 url: get_api_base(),
                 dataType: 'json',
