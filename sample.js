3d89735968c61d36ac945d4da3ffa4bf46856a6e
Russell Fair <rfair404@gmail.com>
2016-03-15 20:35:27 -0400

    cleanup

--- sample.js
+++ sample.js
@@ -40,8 +40,6 @@
         $.ajax({
               url: 'http://rest-api-demo.q21.co/wp-json/',
               dataType: 'json',
-             // jsonp:false, // make it to false, to use your function on JSON RESPONSE
-             //  jsonpCallback: 'response',
               success: function(ret){
                 callback(ret.description);
               }
@@ -52,8 +50,6 @@
        $.ajax({
               url: api_base+'/'+ns,
               dataType: 'json',
-             // jsonp:false, // make it to false, to use your function on JSON RESPONSE
-             //  jsonpCallback: 'response',
               success: function(ret){
                 alert(ret);
                 callback(ret);
@@ -61,122 +57,90 @@
         });
     };
 
-         ext.get_next_post_id = function(callback) {
-          console.log('next posts please');
-            // Make an AJAX call to the WordPress REST API to get site description
-            $.ajax({
-                  url: 'http://rest-api-demo.q21.co/wp-json/wp/v2/posts?per_page=1&page='+post_start+'',
-                  dataType: 'json',
-                 // jsonp:false, // make it to false, to use your function on JSON RESPONSE
-                 //  jsonpCallback: 'response',
-                  success: function(ret){
-                    console.log(ret);
-                    callback(ret[0]['id']);
-                  }
-            });
-            post_start++;
-        };
-
-         ext.get_post_ids = function(callback) {
-          //handle pagination somehow???
-
-        // Make an AJAX call to the WordPress REST API to get a collection of post ids
+    ext.get_next_post_id = function(callback) {
+        // Make an AJAX call to the WordPress REST API to get site description
         $.ajax({
-              url: 'http://rest-api-demo.q21.co/wp-json/wp/v2/posts/',
-              dataType: 'json',
-             // jsonp:false, // make it to false, to use your function on JSON RESPONSE
-             // jsonpCallback: 'response',
+          url: 'http://rest-api-demo.q21.co/wp-json/wp/v2/posts?per_page=1&page='+post_start+'',
+          dataType: 'json',
+          success: function(ret){
+            console.log(ret);
+            callback(ret[0]['id']);
+          }
+        });
+      post_start++;
+    };
 
+    ext.get_post_ids = function(callback) {
+      //handle pagination somehow???
+
+      // Make an AJAX call to the WordPress REST API to get a collection of post ids
+      $.ajax({
+         url: 'http://rest-api-demo.q21.co/wp-json/wp/v2/posts/',
+        dataType: 'json',
               success: function(ret){
                 var post_ids = [];
-
                 for (var i = ret.length - 1; i >= 0; i--) {
                   post_ids.push(ret[i]['id']);
                 };
-              callback(post_ids);
+                callback(post_ids);
               }
         });
-        // postcounter_scratchx = postcounter_scratchx + 1;
     };
+
     ext.get_post_by_id = function(callback) {
-          //handle pagination somehow???
+          // @TODO handle pagination somehow???
           console.log(post_id);
         // Make an AJAX call to the WordPress REST API to get a collection of post ids
         $.ajax({
               url: 'http://rest-api-demo.q21.co/wp-json/wp/v2/posts/'+post_id+'',
               dataType: 'json',
-             // jsonp:false, // make it to false, to use your function on JSON RESPONSE
-             // jsonpCallback: 'response',
-
               success: function(ret){
                 console.log(ret);
                 callback('yepp');
               }
         });
-        // postcounter_scratchx = postcounter_scratchx + 1;
     };
 
 
     ext.set_post = function(post, value) {
-      // console.log(post);
-      // console.log(value);
       post_start = value;
-      console.log('new post value is '+post_start);
+      console.log('New "next post" is '+post_start);
     }
 
     ext.set_api_base = function(api_base, value) {
-      // console.log(post);
-      // console.log(value);
       api_base = value;
       console.log('API base is: '+value);
     }
 
-    ext.set_api_namespace = function(ns, value) {
-      // console.log(post);
-      // console.log(value);
+    ext.set_api_namespace = function(api_namespace, value) {
       api_namespace = value;
       console.log('namespace is: '+value);
     }
 
-    
-
-         ext.get_admin_user = function(callback) {
-
+    ext.get_admin_user = function(callback) {
         // Make an AJAX call to the WordPress REST API to get site description
         $.ajax({
               url: 'http://rest-api-demo.q21.co/wp-json/wp/v2/user/1',
               dataType: 'json',
-             // jsonp:false, // make it to false, to use your function on JSON RESPONSE
-             //  jsonpCallback: 'response',
               success: function(ret){
                 callback(ret.description);
               }
-
-
         });
     };
 
-     ext.access_scratch_dataset = function(data, callback) {
-        // console.log(data);
-        // console.log(callback);
-        // console.log(getVar(foo));
+  	ext.start = function() {
+  		return true;
+  	};
 
-    };
+  	ext.say = function() {
 
-
-	ext.start = function() {
-		return true;
-	};
-
-	ext.say = function() {
-
-	};
+  	};
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['h', 'Enable WordPress REST API', 'start'],
-            [' ', 'Set %m.api_base to %s', 'set_api_base', 'api_base', 'http://'],
+            [' ', 'Set %m.api_base to %s', 'set_api_base', 'api_base', 'http://wp-rest-demo.q21.co/wp-json/'],
             [' ', 'Set %m.api_base to %s', 'set_api_namespace', 'api_namespace', 'wp/v2'],
 
             ['R', 'Site Title', 'get_site_title'],
@@ -193,9 +157,10 @@
         ],
         menus : {
           post_pagination: ['post_next', 'post_previous'],
-          api_base: ['hostname', 'namespace'],
+          api_base: ['api_base', 'api_namespace', 'api_endpoint'],
         },
-        url: 'http://rfair404.github.io/WP-REST-API-FOR-SCRATCHX' // Link to extension documentation, homepage, etc.
+        url: 'http://rfair404.github.io/WP-REST-API-FOR-SCRATCHX' 
+        // Link to extension documentation, homepage, etc.
     };
 
     // Register the extension
